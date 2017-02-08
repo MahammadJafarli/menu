39aa888b62f97755806aa350ca27745ab9222e92
Məhəmməd <mahammad.c@code.edu.az>
2017-02-09 03:40:32 +0400

    menu

---
+++ assets/js/menu.js
@@ -0,0 +1,53 @@
+$('ul.nav li.dropdown').hover(function() {
+      $(this).find('.FirstUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.FirstUl').stop(true, true).slideUp(300);
+   });
+// ==================FIRST UL================================================
+$('.lorem').hover(function() {
+   $(this).find('.SecondUL').stop(true, true).slideDown(300);
+}, function() {
+   $(this).find('.SecondUL').stop(true, true).slideUp(300);
+});
+// ===============SECOND UL==================================================
+   $('.maecenas').hover(function() {
+      $(this).find('.ThirdUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.ThirdUl').stop(true, true).slideUp(300);
+   });
+// ===============Fourth UL==================================================
+   $('.lorem3').hover(function() {
+      $(this).find('.FourthUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.FourthUl').stop(true, true).slideUp(300);
+   });
+// ===============Fifth UL==================================================
+   $('.lorem4').hover(function() {
+      $(this).find('.FifthUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.FifthUl').stop(true, true).slideUp(300);
+   });
+// ===============Sixth UL==================================================
+   $('.lorem5').hover(function() {
+      $(this).find('.SixthUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.SixthUl').stop(true, true).slideUp(300);
+   });
+// ===============Seventh UL==================================================
+   $('.lorem6').hover(function() {
+      $(this).find('.SeventhUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.SeventhUl').stop(true, true).slideUp(300);
+   });
+// ===============Eighth UL==================================================
+   $('.lorem7').hover(function() {
+      $(this).find('.EighthUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.EighthUl').stop(true, true).slideUp(300);
+   });
+// ===============Ninth UL==================================================
+   $('.lorem8').hover(function() {
+      $(this).find('.NinthUl').stop(true, true).slideDown(300);
+   }, function() {
+      $(this).find('.NinthUl').stop(true, true).slideUp(300);
+   });
